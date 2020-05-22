14ccd974934e9205826806da3ad10dfe94361af0
liu164534 <2640044141@qq.com>
2020-05-22 19:18:33 +0800

    mmz

---
+++ mapping-config/src/main/java/com/mmz/config/RedisClusterConfig.java
@@ -0,0 +1,10 @@
+package com.mmz.config;
+
+/**
+ * @program: spring-cloud-mapping
+ * @description: redis集群的配置
+ * @author: Liu Xinpeng
+ * @create: 2020-05-22 19:17
+ **/
+public class RedisClusterConfig {
+}
