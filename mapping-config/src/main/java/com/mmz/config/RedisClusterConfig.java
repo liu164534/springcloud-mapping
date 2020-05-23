ea733c112814c8992ad7fbd6250eea4958b73c08
liu164534 <2640044141@qq.com>
2020-05-23 15:48:11 +0800

    mmz

--- mapping-config/src/main/java/com/mmz/config/RedisClusterConfig.java
+++ mapping-config/src/main/java/com/mmz/config/RedisClusterConfig.java
@@ -1,10 +1,18 @@
 package com.mmz.config;
 
+import com.mmz.properties.RedisClusterProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @program: spring-cloud-mapping
  * @description: redis集群的配置
  * @author: Liu Xinpeng
  * @create: 2020-05-22 19:17
  **/
+@Configuration //定义为配置类，可替换xml文件
 public class RedisClusterConfig {
+
+    @Autowired
+    private RedisClusterProperties redisClusterProperties;
 }
