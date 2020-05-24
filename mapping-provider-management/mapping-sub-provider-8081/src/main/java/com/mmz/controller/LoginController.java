5e2e006dedae5def2365b93870cebaff0fbacd14
liu164534 <2640044141@qq.com>
2020-05-24 17:38:50 +0800

    萌妹子

---
+++ mapping-provider-management/mapping-sub-provider-8081/src/main/java/com/mmz/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.mmz.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: spring-cloud-mapping
+ * @description:
+ * @author: Liu Xinpeng
+ * @create: 2020-05-24 17:36
+ **/
+@RestController
+public class LoginController {
+
+
+}
