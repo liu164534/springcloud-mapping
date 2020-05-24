9673573a648e3483fe62b77311847bb0e948e011
rime-188 <1449068059@qq.com>
2020-05-24 22:34:11 +0800

    测试

---
+++ mapping-management/mapping-service/src/main/java/com/mmz/service/UserService.java
@@ -0,0 +1,24 @@
+package com.mmz.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: springcloud-mapping
+ * @description: 用户服务层
+ * @author: Mr.Miao
+ * @create: 2020-05-24 22:22
+ **/
+@Service
+public class UserService {
+
+    /**
+     * @Description:
+     * @Param: []
+     * @return: boolean
+     * @Author: Mr.miao
+     * @Date: 2020/5/24
+     */
+    public boolean getAllUserInfo() {
+        return false;
+    }
+}
